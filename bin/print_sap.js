7576d56c256342ff924784b616578d4885e7500f
Arne Goedeke <arne@deuso.de>
2019-10-09 14:40:19 +0200

    print_sap: print deletions

--- bin/print_sap.js
+++ bin/print_sap.js
@@ -10,7 +10,12 @@ else
 const announcements = new SAP.Announcements(port);
 
 announcements.on('add', (id, sdp, packet) => {
-  console.log('Received SAP announcements (id=%o) from %s with SDP payload:\n  %s\n',
+  console.log('+++ Received SAP (id=%o) from %s with SDP payload:\n  %s\n',
+              packet.id,
+              packet.source, sdp.replace(/\n/g, '\n  '));
+});
+announcements.on('delete', (id, sdp, packet) => {
+  console.log('--- Received SAP (id=%o) from %s with SDP payload:\n  %s\n',
               packet.id,
               packet.source, sdp.replace(/\n/g, '\n  '));
 });
