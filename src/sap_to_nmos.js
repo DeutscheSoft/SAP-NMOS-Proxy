e9271940813000b0ee80d7c8e806123285ba947d
Arne Goedeke <arne@deuso.de>
2019-10-09 17:46:15 +0200

    Preliminary NMOS sender info for SAP announcement

--- src/sap_to_nmos.js
+++ src/sap_to_nmos.js
@@ -1,3 +1,8 @@
+const crypto = require('crypto');
+const uuid = require('uuid/v5');
+const util = require('util');
+const performance = require('perf_hooks').performance;
+
 const Cleanup = require('./event_helpers.js').Cleanup;
 
 function whenOne(it)
@@ -7,6 +12,24 @@ function whenOne(it)
       p.then(() => resolve(index), (e) => resolve(index));
     });
   });
+}
+
+function SDP_to_NMOS_sender(sdp)
+{
+  // random
+  const PROXY_NAMESPACE = 'd6fe88a0-aac7-4f53-8de3-9046fcc4b766';
+
+  const id = uuid(sdp.id, PROXY_NAMESPACE);
+
+  return {
+    id: id,
+    version: util.format('%d:%d', Date.now(), 0),
+    description: '',
+    label: '',
+    flow_id: id,
+    tags: [ 'sap-proxy' ],
+    transport: 'urn:x-nmos:transport:rtp.mcast',
+  };
 }
 
 function announce_to_registry(sap_announcements, registry)
@@ -22,6 +45,10 @@ function announce_to_registry(sap_announcements, registry)
       console.log('publishing %o to nmos registry %o', sdp.id, registry.url.href);
 
       const update_p = sap_announcements.waitForUpdate(id);
+
+      const info = SDP_to_NMOS_sender(sdp);
+
+      console.log('NMOS: %o', info);
 
       // item was deleted
       switch (await whenOne([ update_p, delete_p, cleanup_p ]))
