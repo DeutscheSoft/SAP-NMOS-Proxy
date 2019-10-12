58a19f4c4b564f66a37ebead1073c71068432eb8
Arne Goedeke <arne@deuso.de>
2019-10-12 19:34:12 +0200

    NMOS: added datastructures and flow for device registration

--- bin/nmos_node.js
+++ bin/nmos_node.js
@@ -1,10 +1,27 @@
+const uuid = require('uuid/v5');
+const util = require('util');
+
 const Node = require('../src/nmos/node.js');
+
+const node_id = 'd6fe88a0-aac7-4f53-8de3-9046fcc4b766';
 
 const node = new Node({
   http_port: 1080,
   info: {
-    id: 'd6fe88a0-aac7-4f53-8de3-9046fcc4b766',
+    id: node_id,
     label: 'SAP-to-NMOS proxy',
     description: 'This node proxies between SAP and NMOS.',
   },
+});
+
+const device = node.makeDevice({
+  id: uuid('device:192.168.178.134', node_id),
+  version: util.format('%d:%d', Date.now(), 0),
+  label: 'my device',
+  description: '',
+  tags: {},
+  type: "urn:x-nmos:device:audio",
+  senders: [],
+  receivers: [],
+  controls: [],
 });
--- src/nmos/node.js
+++ src/nmos/node.js
@@ -1,24 +1,253 @@
-const RegistryResolver = require('./discovery.js').RegistryResolver;
-const connect = require('connect');
+const assert = require('assert');
+const Events = require('events');
 const util = require('util');
 const http = require('http');
+
+const connect = require('connect');
 const dnssd = require('dnssd');
 
-class Sender
+const Cleanup = require('../event_helpers.js').Cleanup;
+const DynamicSet = require('../dynamic_set.js');
+const RegistryResolver = require('./discovery.js').RegistryResolver;
+
+function whenOne(it)
 {
+  return new Promise((resolve, reject) => {
+    it.forEach((p, index) => {
+      p.then(() => resolve(index), (e) => resolve(index));
+    });
+  });
 }
 
-class Device
+function deep_equal(a, b)
 {
-  constructor(node, info)
+  try
   {
-    this.node = node;
-    this.senders = new Map();
+    assert.deepStrictEqual(a, b, '');
+    return true;
+  }
+  catch (e)
+  {
+    return false;
+  }
+}
+
+function delay(timeout)
+{
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+async function retry(task, n, timeout)
+{
+  let err;
+
+  if (!n) n = 3;
+  if (!timeout) timeout = 1000;
+
+  for (let i = 0; i < n; i++)
+  {
+    try
+    {
+      return await task();
+    }
+    catch (e)
+    {
+      err = e;
+      await delay(timeout);
+    }
   }
 
-  makeSender(info)
-  {
+  throw err;
+}
 
+class Resource extends Events
+{
+  get id()
+  {
+    return this.info.id;
+  }
+
+  constructor(parent, info)
+  {
+    super();
+    this.parent = parent;
+    this.info = Object.assign({}, info);
+  }
+
+  update(info)
+  {
+    if (info.id && info.id !== this.id)
+      throw new Error('ID cannot be changed.');
+
+    const n = Object.assign({}, this.info, info);
+    if (deep_equal(n, this.info))
+      return;
+    this.info = n;
+    this.emit('update');
+  }
+
+  close()
+  {
+    this.emit('close');
+  }
+
+  registerSelf(api)
+  {
+    return Promise.reject(new Error('Not implemented.'));
+  }
+
+  unregisterSelf(api)
+  {
+    return Promise.reject(new Error('Not implemented.'));
+  }
+
+  startChildRegistration(api)
+  {
+    // nothing to do on cleanup.
+    return () => {};
+  }
+
+  startRegistration(api)
+  {
+    const cleanup = new Cleanup();
+
+    cleanup.whenClosed().then(async () => {
+      try
+      {
+        await unregisterSelf(api);
+      } catch (err) {
+        console.error('Failed to remove device: %o', this.id);
+      }
+    });
+
+    this.on('close', () => cleanup.close());
+
+    (async () => {
+      await retry(() => this.registerSelf(api), 3, 1000);
+
+      cleanup.subscribe(this, 'update', async () => {
+        try
+        {
+          await retry(() => registerSelf(api), 3, 1000);
+        }
+        catch (err)
+        {
+          console.error('Update of %o failed: %o', this.info, err);
+          cleanup.close();
+        }
+      });
+
+      // start registering children.
+      cleanup.add(this.startChildRegistration(api));
+    })().catch((err) => {
+      console.error('Registration of %o failed: %o', this.info, err);
+      cleanup.close();
+    });
+
+    return cleanup;
+  }
+}
+
+class ResourceSet extends DynamicSet
+{
+  constructor(type, parent)
+  {
+    super();
+    this.type = type;
+    this.parent = parent;
+  }
+
+  create(info)
+  {
+    const id = info.id;
+
+    if (this.has(id))
+    {
+      throw new Error("Resource with given id already exists.");
+    }
+
+    const resource = new this.type(this.parent, info);
+
+    resource.on('close', () => {
+      this.delete(id);
+    });
+
+    this.add(id, resource);
+
+    return resource;
+  }
+
+  make(info)
+  {
+    const resource = this.get(info.id);
+
+    if (!resource)
+      return this.create(info);
+
+    resource.update(info);
+
+    return resource;
+  }
+
+  startRegistration(api)
+  {
+    const cleanup = new Cleanup();
+
+    console.log('Registering set');
+
+    cleanup.add(this.forEachAsync((entry, id) => {
+      console.log('device: %o', entry);
+      cleanup.add(entry.startRegistration(api));
+    }));
+
+    return cleanup;
+  }
+}
+
+class Sender extends Resource
+{
+  get device()
+  {
+    return this.parent;
+  }
+}
+
+class Senders extends ResourceSet
+{
+  constructor(device)
+  {
+    super(Sender, device);
+  }
+}
+
+class Device extends Resource
+{
+  get node()
+  {
+    return this.parent;
+  }
+
+  constructor(node, info)
+  {
+    super(node, info);
+    this.senders = new Senders();
+  }
+
+  registerSelf(api)
+  {
+    return api.registerDevice(this.info);
+  }
+
+  unregisterSelf(api)
+  {
+    return api.deleteDevice(this.info);
+  }
+
+  startChildRegistration(api)
+  {
+    return this.senders.startRegistration(api);
   }
 }
 
@@ -46,7 +275,15 @@ function get_first_public_ip(family)
   }
 }
 
-class Node
+class Devices extends ResourceSet
+{
+  constructor(node)
+  {
+    super(Device, node);
+  }
+}
+
+class Node extends Resource
 {
   constructor(options)
   {
@@ -79,9 +316,12 @@ class Node
       interface: ip,
     }, options.dnssd || {});
 
-    this.info = info;
+    super(null, info);
+
     this.advertisement = new dnssd.Advertisement(dnssd.tcp('nmos-node'), http_port);
     this.resolver = new RegistryResolver(dnssd_options);
+    this.cleanup = new Cleanup();
+    this.devices = new Devices();
 
     const app = connect().use('/self', (req, res, next) => {
       console.log("got request for /self");
@@ -89,15 +329,52 @@ class Node
     });
 
     this.http = http.createServer(app).listen(http_port, ip);
-    const register = () => {
-      this.resolver.forEach((api, url) => {
-        api.registerNode(this.info);
-      });
-    };
 
-    register();
+    this.cleanup.add(this.resolver.forEachAsync((api, url) => {
+      this.cleanup.add(this.startRegistration(api));
+    }));
+  }
 
-    this.heartbeat_id = setInterval(register, 5000);
+  registerSelf(api)
+  {
+    return api.registerNode(this.info);
+  }
+
+  unregisterSelf(api)
+  {
+    return api.deleteNode(this.info);
+  }
+
+  startChildRegistration(api)
+  {
+    const cleanup = new Cleanup();
+
+    console.log('registering children.');
+
+    let interval_id = setInterval(() => this.registerSelf(api), 5000);
+
+    cleanup.add(() => {
+      clearInterval(interval_id);
+    });
+    cleanup.add(this.devices.startRegistration(api));
+
+    return cleanup;
+  }
+
+  createDevice(info)
+  {
+    info = Object.assign({}, info, {
+      node_id: this.id,
+    });
+    return this.devices.create(info);
+  }
+
+  makeDevice(info)
+  {
+    info = Object.assign({}, info, {
+      node_id: this.id,
+    });
+    return this.devices.make(info);
   }
 
   close()
@@ -105,7 +382,7 @@ class Node
     this.resolver.close();
     this.http.close();
     this.advertisement.close();
-    clearInterval(this.heartbeat_id);
+    this.cleanup.close();
   }
 }
 
