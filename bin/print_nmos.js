25716aef14f10ef4dde60ca5080e292015d466bf
Arne Goedeke <arne@deuso.de>
2019-10-15 16:31:27 +0200

    bin/print_nmos: combine senders from all sources

--- bin/print_nmos.js
+++ bin/print_nmos.js
@@ -1,27 +1,40 @@
 const nmos = require('../src/nmos.js');
+const UnionSet = require('../src/dynamic_set.js').UnionSet;
 
 const queries = new nmos.Discovery.QueryResolver();
 const registries = new nmos.Discovery.RegistryResolver();
 const nodes = new nmos.Discovery.NodeResolver();
 
-queries.forEachAsync((api, url) => {
-  const senders = api.senders();
+const queries_and_nodes = queries.union(nodes);
 
-  console.log('registry found: %o', url);
+const senders = new UnionSet();
 
-  senders.forEachAsync(async (sender, id) => {
-    console.log('sender found in registry: %o', sender);
-    console.log('SDP: %s', await sender.fetchManifest());
+queries_and_nodes.forEachAsync((api, id, set) => {
+  let _senders;
+
+  console.log('api found at: %s', api.url);
+
+  const start = () => {
+    api = set.get(id);
+    _senders = api.senders();
+    senders.addSet(_senders);
+  };
+  const stop = () => {
+    senders.removeSet(_senders);
+    _senders.close();
+    _senders = null;
+    api = null;
+  };
+
+  set.waitForUpdate(id).then(() => {
+    stop();
+    start();
   });
+  set.waitForDelete(id).then(stop);
+  start();
 });
 
-nodes.forEachAsync((api, url) => {
-  const senders = api.senders();
-
-  console.log('node found: %o', url);
-
-  senders.forEachAsync(async (sender, id) => {
-    console.log('sender found in node: %o', sender);
-    console.log('SDP: %s', await sender.fetchManifest());
-  });
+senders.forEachAsync(async (sender, id) => {
+  console.log('sender found in registry: %o', sender);
+  console.log('SDP: %s', await sender.fetchManifest());
 });
