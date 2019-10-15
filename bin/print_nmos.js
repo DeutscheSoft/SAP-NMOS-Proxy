c08d02af348233435079e65200a9a9231878d1b4
Arne Goedeke <arne@deuso.de>
2019-10-15 13:08:52 +0200

    bin/print_nmos: include node information

--- bin/print_nmos.js
+++ bin/print_nmos.js
@@ -2,6 +2,7 @@ const nmos = require('../src/nmos.js');
 
 const queries = new nmos.Discovery.QueryResolver();
 const registries = new nmos.Discovery.RegistryResolver();
+const nodes = new nmos.Discovery.NodeResolver();
 
 queries.forEachAsync((api, url) => {
   const senders = api.senders();
@@ -9,7 +10,18 @@ queries.forEachAsync((api, url) => {
   console.log('registry found: %o', url);
 
   senders.forEachAsync(async (sender, id) => {
-    console.log('sender found: %o', sender);
+    console.log('sender found in registry: %o', sender);
+    console.log('SDP: %s', await sender.fetchManifest());
+  });
+});
+
+nodes.forEachAsync((api, url) => {
+  const senders = api.senders();
+
+  console.log('node found: %o', url);
+
+  senders.forEachAsync(async (sender, id) => {
+    console.log('sender found in node: %o', sender);
     console.log('SDP: %s', await sender.fetchManifest());
   });
 });
