bafe67e06c2f5c6f05d2823a7fb931f47c6b42ce
Arne Goedeke <arne@deuso.de>
2019-10-16 15:48:04 +0200

    bin/print_nmos: use AllSenders

--- bin/print_nmos.js
+++ bin/print_nmos.js
@@ -1,38 +1,7 @@
-const nmos = require('../src/nmos.js');
+const NMOS = require('../src/nmos.js');
 const UnionSet = require('../src/dynamic_set.js').UnionSet;
 
-const queries = new nmos.Discovery.QueryResolver();
-const registries = new nmos.Discovery.RegistryResolver();
-const nodes = new nmos.Discovery.NodeResolver();
-
-const queries_and_nodes = queries.union(nodes);
-
-const senders = new UnionSet();
-
-queries_and_nodes.forEachAsync((api, id, set) => {
-  let _senders;
-
-  console.log('api found at: %s', api.url);
-
-  const start = () => {
-    api = set.get(id);
-    _senders = api.senders();
-    senders.addSet(_senders);
-  };
-  const stop = () => {
-    senders.removeSet(_senders);
-    _senders.close();
-    _senders = null;
-    api = null;
-  };
-
-  set.waitForUpdate(id).then(() => {
-    stop();
-    start();
-  });
-  start();
-  return stop;
-});
+const senders = new NMOS.Discovery.AllSenders();
 
 senders.forEachAsync(async (sender, id) => {
   console.log('sender found in registry: %o', sender);
