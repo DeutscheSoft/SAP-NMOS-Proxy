13d3f6fcdddd47cabf1749c905e146c3b735040b
Arne Goedeke <arne@deuso.de>
2019-10-21 11:13:03 +0200

    export SDP

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = Object.assign({
   SAP: require('./src/sap.js'),
   Proxy: require('./src/proxy.js'),
   NMOS: require('./src/nmos.js'),
+  SDP: require('./src/sdp.js'),
 }, require('./src/dynamic_set.js'), require('./src/event_helpers.js'));
