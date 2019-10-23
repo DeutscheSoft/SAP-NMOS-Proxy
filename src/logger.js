9608e66ff42433033ba442c0501687f299adf986
Arne Goedeke <arne@deuso.de>
2019-10-23 23:07:05 +0200

    Log: added logging levels

--- src/logger.js
+++ src/logger.js
@@ -15,5 +15,7 @@ Log.error = Log.bind(null, 0);
 Log.warn = Log.bind(null, 1);
 Log.info = Log.bind(null, 2);
 Log.log = Log.bind(null, 3);
+Log.verbose = Log.bind(null, 4);
+Log.annoy = Log.bind(null, 5);
 
 module.exports = Log;
