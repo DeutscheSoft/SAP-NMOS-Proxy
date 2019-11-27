8f733116cd8ae6fb53a68b2434b2ccf30c108250
Arne Goedeke <arne@deuso.de>
2019-11-27 18:13:24 +0100

    Proxy: allow passing sdp files for testing

--- bin/proxy.js
+++ bin/proxy.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
 const argparse = require('argparse');
 const uuid = require('uuid/v5');
 
@@ -7,6 +9,7 @@ const Interfaces = require('../src/interfaces.js').Interfaces;
 const Proxy = require('../').Proxy;
 
 const Log = require('../src/logger.js');
+const SDP = require('../src/sdp.js');
 
 const argumentParser = new argparse.ArgumentParser({
   version: '0.0.1',
@@ -25,6 +28,13 @@ argumentParser.addArgument(
   [ '-l', '--loglevel' ],
   {
     help: 'Set log level. 0: errors, 1: warnings, 2: info, 3: debug.',
+  }
+);
+
+argumentParser.addArgument(
+  [ '--sdp-file' ],
+  {
+    help: 'Comma seperated list of SDP files. They are added to the proxy.',
   }
 );
 
@@ -47,6 +57,18 @@ const cleanup = interfaces.filter(interface_filter).forEachAsync((network_interf
   const proxy = new Proxy({
     interface: network_interface,
   });
+
+  if (args.sdp_file)
+  {
+    args.sdp_file.split(',').forEach((fname) => {
+      const sdp_file = fs.readFileSync(fname, 'utf8');
+      const sdp = new SDP(sdp_file);
+
+      Log.info('Using SDP file %o', fname);
+
+      proxy.sdpStringsToNMOS.add(sdp.id, sdp);
+    });
+  }
 
   proxy.on('log', (...args) => {
     console.log(...args);
