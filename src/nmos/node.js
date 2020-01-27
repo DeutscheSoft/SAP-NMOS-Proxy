13fc6c534ebd9d0275a241cf068c9bb8281c6a57
Arne Goedeke <arne@deuso.de>
2020-01-27 11:11:30 +0100

    NMOS.Node: trigger node update when clock or interface are added
    This should help to make sure that clocks and interfaces are correctly
    registered.

--- src/nmos/node.js
+++ src/nmos/node.js
@@ -1147,6 +1147,7 @@ class Node extends Resource
     const clock = new Clock(this, info);
 
     this.clocks.set(name, clock);
+    this.triggerUpdate();
 
     clock.on('close', () => {
       this.clocks.delete(name);
@@ -1210,6 +1211,7 @@ class Node extends Resource
     const iface = new Interface(this, info);
 
     this.interfaces.set(name, iface);
+    this.triggerUpdate();
 
     iface.on('close', () => {
       this.interfaces.delete(name);
